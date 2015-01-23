9789e5959f5b72c837521a8e4c78259793a98948
emadera52 <emadera52@hotmail.com>
2015-01-22 19:46:27 -0600

    Update comments

--- main.go
+++ main.go
@@ -12,18 +12,22 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-// Init resgister the sixtyplus database with Beego's ORM
-// IMPORTANT: Edit sixty/conf/app.conf changing 'mysqluser' and 'mysqlpass'
-// to the values you use to access your MySQL database
+// Init resgisters the sixtyplus database with Beego's ORM
+// IMPORTANT: Edit sixty/conf/app.conf changing 'mysqluser', 'mysqlpass'
+// and 'mysqldb' to the values you use to access your MySQL database
 // If the DB is not on the app's server at localhost:3306, edit
 // the information between '@tcp(' and ')/' as appropriate
 func init() {
 	orm.RegisterDriver("mysql", orm.DR_MySQL)
 	mysqlReg := beego.AppConfig.String("mysqluser") + ":" +
-		beego.AppConfig.String("mysqlpass") + "@tcp(127.0.0.1:3306)/sixtyplus"
+		beego.AppConfig.String("mysqlpass") + "@tcp(127.0.0.1:3306)/" +
+		beego.AppConfig.String("mysqldb")
 	orm.RegisterDataBase("default", "mysql", mysqlReg)
 }
 
+// Main is the program's entry point after all imports and
+// related init functions have been loaded/executed
+// Explicit Beego options can set here before calling Beego.Run()
 func main() {
 	beego.SessionOn = true
 	beego.Run()
